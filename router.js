4682979971abd2af8450263fac3aafe0c9599485
Anwesh43 <anweshthecool0@gmail.com>
2018-02-03 18:08:30 +0530

    sending error data to server

--- router.js
+++ router.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 router.post('/log-error',(req,res)=>{
     const body = req.body
     const url = body.url
-    const errName = body.errName
+    const errorMsg = body.errorMsg
     const lineNumber = body.lineNumber
     console.log(body)
-    res.json({url,errName,lineNumber})
+    res.json({url,errorMsg,lineNumber})
 })
 module.exports = router
